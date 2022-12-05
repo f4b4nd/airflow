34a4f53502fb017a7c27acaa0d50408b2607a9e8
fabien <fabien.andrian@gmail.com>
2022-12-05 21:55:19 +0100

    minor

--- dags/sursaut_covid.py
+++ dags/sursaut_covid.py
@@ -7,7 +7,7 @@ from airflow.providers.jdbc.operators.jdbc import JdbcOperator
 
 @dag(
 	dag_id="sursaut_covid_2",
-	# schedule_interval="0 0 * * *",
+	schedule_interval="0 0 * * *",
 	start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
 	catchup=False,
 	dagrun_timeout=datetime.timedelta(minutes=60),
@@ -16,7 +16,7 @@ def Process():
 
     read_table_jdbc = JdbcOperator(
         task_id="jdbc_fetchall",
-        jdbc_conn_id="jdbc_inpa",
+        jdbc_conn_id="etlv2_inpa",
         sql=" select * from etlv2.sursaut_covid ",
         autocommit=True,
     )
