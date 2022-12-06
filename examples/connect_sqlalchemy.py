d8a7c9c08636c201a21646623dc36da5abcf02d8
fabien.andrian@gmail.com <fabien.andrian@gmail.com>
2022-12-06 18:44:08 +0100

    working example

--- examples/connect_sqlalchemy.py
+++ examples/connect_sqlalchemy.py
@@ -1,5 +1,5 @@
 import os, sys
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 
 username = os.getenv('USERNAME', '')
 password = os.getenv('PASSWORD', '')
@@ -9,12 +9,18 @@ ip = "1521"
 sid = "INPA" 
 
 if __name__ == "__main__":
-    url = f"jdbcapi+oraclejdbc://{username}/{password}@{host}:{ip}/{sid}"
+    url = f"jdbcapi+oraclejdbc://{host}:{ip}/{sid}"
     print(url)
     print(url.split("//", 1)[-1])
-    sys.exit()
+    #sys.exit()
     engine = create_engine(url)
     with engine.connect() as connection:
         result = connection.execute("select * from exemple")
         for row in result:
             print(row)
+
+
+# working with :
+url = f"jdbcapi+oraclejdbc://{host}:{ip}/{sid}"
+# "url" : "jdbc:oracle:thin:@egp-sdlbinpa2.egp.aphp.fr:1521:INPA",
+# "driver_args": ['etlv2', 'pwd']
